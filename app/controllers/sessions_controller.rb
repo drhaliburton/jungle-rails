f01ff7fb6a74786a0a54f8700d0544750924eb11
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-07 16:45:20 +0000

    Fixed quantities/total price calcs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # puts params
     user = User.find_by_email(params[:user][:email])
-    # puts user
     if user && user.authenticate(params[:user][:password])
-      # puts "sure, yeah, no problem"
       session[:user_id] = user.id
       redirect_to '/'
     else
-      # puts "nope"
       redirect_to '/'
     end
   end
