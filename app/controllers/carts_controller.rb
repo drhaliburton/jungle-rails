c5fd940af0a6a16a0b1666ead8dca52c185175a0
Rebecca Haliburton <haliburtonre@gmail.com>
2017-07-09 21:07:33 +0000

    Added order total formatting for receipt email

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,9 @@
 class CartsController < ApplicationController
 
-  before_filter :authorize
-
   def show
     @user = current_user
+    @user_name = show_username
+    @user_email = show_email
   end
 
   def add_item
@@ -27,6 +27,22 @@ class CartsController < ApplicationController
     update_cart cart
 
     redirect_to :back
+  end
+
+  def show_username
+    if current_user
+      current_user.first_name
+    else
+      'Guest'
+    end
+  end
+
+  def show_email
+    if current_user
+      current_user.email
+    else
+      'Guest@guest.ca'
+    end
   end
 
 end
